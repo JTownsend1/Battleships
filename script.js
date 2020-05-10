ab46eb10aecc32638b9c4478f46fb8d9d7782803
Jonathon Townsend <jtownsend@outlook.com>
2020-05-10 19:10:05 +0100

    Placement check and edge check condition functions added

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var squareSize = 50;
 let grid = []; 
 let id = 0;
 let idd = 0;
+let shipOrientation = 0;
 
 class cell {
     constructor(id, row, col, state) {
@@ -40,38 +41,72 @@ createGrid();
 
 
 function placeShip() {
-    let s = new ship(0, Math.floor(Math.random() * gridSize), 3, "Submarine")
+    // let s = new ship(Math.floor(Math.random() * 4), Math.floor(Math.random() * gridSize), 4, "Submarine")
+    let s = new ship(0, Math.floor(Math.random() * gridSize), 4, "Submarine")
 
-            // if (grid[s.startPosition] + grid[s.startPosition].shipSize > )
+        // Randomly choose a starting square
+        // Determine starting orientation
+        // Check that the squares that a ship is on are not already used
+        // Check that the ships do not cross the edges of the grid
+        // Then if all of the above is OK, place ship / change states of each cell
 
-            if (grid[s.startPosition].state == 0) {
-                for (i = 0; i < s.shipSize; i++) {
-                    grid[s.startPosition].state = 1;
-                    grid[s.startPosition + (i*10)].state  = 1;
-                }
-
-                
-               
-                
-            // if (orientation === 0) {
-            //     if 
-            // }   
             
-        }
+            //// DETERMINE STARTING ORIENTATION
+            if (s.orientation == 0) { shipOrientation = cols};  // RIGHT
+            if (s.orientation == 1) { shipOrientation = 1};  // DOWN  
+            if (s.orientation == 2) { shipOrientation = -cols};  // LEFT
+            if (s.orientation == 3) { shipOrientation = -1};  // UP
+            
+            // CHECKS STARTING POSITION
+            function checkPlacement() {
+                for (i = 0; i < s.shipSize; i++) {
+                    if (grid[s.startPosition + (i * shipOrientation)].state  == 0) {
+                    return true;
+                    }
+                }
+            }
+
+            // CHECK EDGES
+            function checkEdges() {
+                for (i = 0; i < s.shipSize; i++) {
+                    if (grid[s.startPosition + (i * shipOrientation)].id-1 < gridSize) {
+                    return true;   
+                    }
+                }  
+            }
+
+            
+if (checkEdges() == true) {console.log('Placement squares unnoccupied')};
+if (checkEdges() == true) {console.log('All within edges')};
+
+
+
+if (grid[s.startPosition].state == 0) {
+    for (i = 0; i < s.shipSize; i++) {
+        grid[s.startPosition].state = 1;
+        grid[s.startPosition + (i * shipOrientation)].state = 1;
+    }
 }
 
 
-
-
-
+}
 
 placeShip()
 
+// function check() {
+//     if (rows == 100) {
+//         return true;
+//     }
+// }
+
+// check();
+
+// if (check()==true) {
+//     console.log('YES')
+// }
 
 
-
-
-//VIEW
+//// VIEW
 
 function createGrid1() {
         let gameBoardContainer = document.getElementById("gameboard");
@@ -92,8 +127,8 @@ function createGrid1() {
                 gameBoardContainer.appendChild(sq);
 		        
 
-                console.log(idd);
-                console.log(grid[idd].state);
+                // console.log(idd);
+                // console.log(grid[idd].state);
                 idd++;
 
                 var topPosition = j * squareSize;
